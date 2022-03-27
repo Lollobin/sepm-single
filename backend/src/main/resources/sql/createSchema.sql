77d6032cb29dd1d748419d073efa4e01d097333b
BuildTools <unconfigured@null.spigotmc.org>
2022-03-27 11:52:52 +0200

    [30] added foreign key constraints to database

--- backend/src/main/resources/sql/createSchema.sql
+++ backend/src/main/resources/sql/createSchema.sql
@@ -1,3 +1,11 @@
+CREATE TABLE IF NOT EXISTS owner
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    firstName   VARCHAR(255) NOT NULL,
+    lastName    VARCHAR(255) NOT NULL,
+    email       VARCHAR(255) NULL
+);
+
 CREATE TABLE IF NOT EXISTS horse
 (
     id          BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -7,13 +15,8 @@ CREATE TABLE IF NOT EXISTS horse
     sex         ENUM ('male', 'female') NOT NULL,
     ownerId     BIGINT NULL,
     fatherId    BIGINT NULL,
-    motherId    BIGINT NULL
-);
-
-CREATE TABLE IF NOT EXISTS owner
-(
-    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    firstName   VARCHAR(255) NOT NULL,
-    lastName    VARCHAR(255) NOT NULL,
-    email       VARCHAR(255) NULL
+    motherId    BIGINT NULL,
+    CONSTRAINT ownerId FOREIGN KEY (ownerId) REFERENCES owner(id) ON DELETE SET NULL,
+    CONSTRAINT fatherId FOREIGN KEY (fatherId) REFERENCES horse(id) ON DELETE SET NULL,
+    CONSTRAINT motherId FOREIGN KEY (motherId) REFERENCES horse(id) ON DELETE SET NULL
 );
